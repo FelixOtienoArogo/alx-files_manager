46dca2050f4bab8db91c752c5a0e242d01429317
FelixOtienoArogo <felixarogo@gmail.com>
2023-06-24 11:34:00 +0300

    fixing lint

--- utils/redis.js
+++ utils/redis.js
@@ -1,31 +1,31 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
-class RedisClient{
- constructor() {
-  this.client = createClient();
-  this.connect = true;
-  this.client.on('error', (err) => {
-	  this.connect = false;
-	  console.log(err)
-  });
- }
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.connect = true;
+    this.client.on('error', (err) => {
+      this.connect = false;
+      console.log(err);
+    });
+  }
 
- isAlive(){
-  return this.connect;
- }
+  isAlive() {
+    return this.connect;
+  }
 
- async get(key){
-  return promisify(this.client.get).bind(this.client)(key);
- }
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
 
- async set(key, value, duration){
-  await promisify(this.client.setex).bind(this.client)(key, duration, value);
- }
+  async set(key, value, duration) {
+    await promisify(this.client.setex).bind(this.client)(key, duration, value);
+  }
 
- async del(key){
-  await promisify(this.client.del).bind(this.client)(key);
- }
+  async del(key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
 }
 
 export const redisClient = new RedisClient();
